cb8bc811ea7341655350df3fdacd6cc41a6728f4
grarizki <28901232+grarizki@users.noreply.github.com>
2022-09-19 15:35:20 +0700

    init L3

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const product = 'Socks'
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Sendal Swallow',
-            description: 'Hidup jangan sekedar hidup'
+            product: 'Socks'
         }
     }
 })
