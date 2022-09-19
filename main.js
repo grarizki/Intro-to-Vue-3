3c47262013368dbc56d1dc1e9d0ad773c91e6b7a
grarizki <28901232+grarizki@users.noreply.github.com>
2022-09-19 15:27:15 +0700

    add product and description object

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Sendal Swallow',
+            description: 'Hidup jangan sekedar hidup'
+        }
+    }
+})
