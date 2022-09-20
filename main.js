cf6124e1f1e37fc08d00f390d7aabcb307174f82
grarizki <28901232+grarizki@users.noreply.github.com>
2022-09-20 15:18:17 +0700

    finish code challenge with key

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [{ id: 2234, color: 'green' }, { id: 2234, color: 'green' },]
+            variants: [{ id: 2234, color: 'green' }, { id: 2234, color: 'green' },],
+            sizes: ['S', 'M', 'L', 'XL'],
         }
     }
 })
